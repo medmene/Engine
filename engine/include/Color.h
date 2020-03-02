f5e2fc6dfab0fc3091ca81100eb352f6349ff8a1
medmene <medmene@github.com>
2020-03-02 21:10:19 +0700

    fix color privacy

--- engine/include/Color.h
+++ engine/include/Color.h
@@ -3,14 +3,19 @@
 class Color
 {
 public:
-	int r;
-	int g;
-	int b;
-	int a;
-
 	Color();
 	Color(const Color & other);
 	Color(int _r, int _g, int _b, int _a);
+
+	void SetR(int _r) { r = _r; fixupColor(); }
+	void SetG(int _g) { g = _g; fixupColor(); }
+	void SetB(int _b) { b = _b; fixupColor(); }
+	void SetA(int _a) { a = _a; fixupColor(); }
+
+	int GetR() { return r; }
+	int GetG() { return g; }
+	int GetB() { return b; }
+	int GetA() { return a; }
 
 	Color& operator=(const Color& other);
 	bool operator==(const Color& other) const;
@@ -24,5 +29,10 @@ public:
 	Color& operator/=(int other);
 	Color& operator/=(float other);
 private:
+	int r;
+	int g;
+	int b;
+	int a;
+
 	void fixupColor();
 };
